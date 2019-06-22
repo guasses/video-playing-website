6944ac6b034e2da9cdea27f51dfbd9104990d37f
hezihu <hezihu@foxmail.com>
2019-06-22 19:37:13 +0800

    加入登陆页面

---
+++ js/cross-browser.js
@@ -0,0 +1,109 @@
+var EventUtil = {
+
+    //添加事件
+    addHandler:function(element,type,handler){
+        if(element.addEventListener){
+            element.addEventListener(type,handler,false);
+        }else if(element.attachEvent){
+            element.attachEvent("on" + type,handler);
+        }else{
+            element["on" + type] = handler;
+        }
+    },
+    //获取event
+    getEvent:function(event){
+        return event ? event : window.event;
+    },
+    //获取事件的目标
+    getTarget:function(event){
+        return event.target || event.srcElement;
+    },
+    //取消事件的默认行为
+    preventDefault:function(event){
+        if(event.preventDefault){
+            event.preventDefault();
+        }else{
+            event.returnValue = false;
+        }
+    },
+    //获取相关元素，mouseover、mouseout的相关元素
+    getRelatedTarget:function(event){
+        if(event.relatedTarget){
+            return event.relatedTarget;
+        }else if(event.toElement){
+            return event.toElement;
+        }else if(event.fromElement){
+            return event.fromElement;
+        }else{
+            return null;
+        }
+    },
+    //获取鼠标按钮事件
+    getButton:function(event){
+        if(document.implementation.hasFeature("MouseEvents","2.0")){
+            return event.button;
+        }else{
+            switch(event.button){
+                case 0:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    return 0;
+                case 2:
+                case 6:
+                    return 2;
+                case 4:
+                    return 1;
+            }
+        }
+    },
+    //获取鼠标滚轮增量值,client对象在第9章
+    getWheelDelta:function(event){
+        if(event.wheelDelta){
+            return (client.engine.opera && client.engine.opera < 9.5 ? 
+                -event.wheelDelta : event.wheelDelta);
+        }else{
+            return -event.detail * 40;
+        }
+    },
+    //获取键码
+    getCharCode:function(event){
+        if(typeof event.charCode == "number"){
+            return event.charCode;
+        }else{
+            return event.keyCode;
+        }
+    },
+    //获取剪切板数据
+    getClipboardText:function(event){
+        var clipboardData = (event.clipboardData || window.clipboardData);
+        return clipboardData.getData("text");
+    },
+    //设置剪切板数据
+    setClipboardText:function(event,value){
+        if(event.clipboardData){
+            return event.clipboardData.setData("text/plain",value);
+        }else if(window.clipboardData){
+            return window.clipboardData.setData("text",value);
+        }
+    },
+    //移除事件
+    removeHandler:function(element,type,hanlder){
+        if(element.removeEventListener){
+            element.removeEventListener(type,handler,false);
+        }else if(element.detachEvent){
+            element.detachEvent("on"+type,handler);
+        }else{
+            element["on"+type]=null;
+        }
+    },
+    //停止冒泡
+    stopPropagation:function(event){
+        if(event.stopPropagation){
+            event.stopPropagation();
+        }else{
+            event.cancelBubble = true;
+        }
+    }
+};
